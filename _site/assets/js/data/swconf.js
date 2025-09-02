9195fe7d1b579a85c341dcb5e1877e216a9a863c
whoami <diogo.m.lages@gmail.com>
2025-09-02 01:34:13 +0100

    removed some posts

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743368015',resources: [
+    cacheName: 'chirpy-1756773217',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
